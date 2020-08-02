afaf856d1121967dfe9ef43421ea087751f84032
StanislavVasilev <svasilievalee@gmail.com>
2020-08-03 00:50:24 +0300

    "Добавлен класс проперти"

--- src/main/java/managers/PropertyManager.java
+++ src/main/java/managers/PropertyManager.java
@@ -1,4 +1,27 @@
 package managers;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class PropertyManager {
+  public static Properties properties = new Properties();
+
+  private PropertyManager INSTANCE = null;
+
+  public PropertyManager getProperty() {
+    try {
+      properties.load(new FileInputStream("src/main/resources",
+              properties.getProperty(("key", "value"), "environment"));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public PropertyManager getINSTANCE() {
+    if (INSTANCE == null) {
+      INSTANCE = new PropertyManager();
+    }
+    return INSTANCE;
+  }
+
 }
