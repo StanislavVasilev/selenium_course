8ce1203eee24615169830be3a29870e5a3543cbe
StanislavVasilev <svasilievalee@gmail.com>
2020-08-03 01:05:59 +0300

    "Реализован драйвер менеджер"

--- src/main/java/managers/DriverManager.java
+++ src/main/java/managers/DriverManager.java
@@ -5,27 +5,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverManager {
-  private final PropertyManager property = new PropertyManager().getInstance();
-  WebDriver driver;
+  private static final PropertyManager property = new PropertyManager().getInstance();
+  private static WebDriver driver;
 
-  public WebDriver initDriver() {
+  private static WebDriver initDriver() {
     switch (property.getProperty("browser")) {
       case "firefox":
+        System.setProperty("", "");
         driver = new FirefoxDriver();
         break;
       default:
+        System.setProperty("", "");
         driver = new ChromeDriver();
         break;
     }
     return driver;
   }
 
-  public WebDriver getDriver() {
-    return initDriver();
+  public static WebDriver getDriver() {
+    if (driver==null){
+      initDriver();
+    }
+    return driver;
   }
 
   public void stopDriver() {
     driver.quit();
-    driver == null;
+    driver = null;
   }
 }
