21f114361cff19836932ebf0c2fbb40cdcb1a96d
StanislavVasilev <svasilievalee@gmail.com>
2020-08-03 00:22:32 +0300

    "Итоговое задание"

---
+++ src/main/java/managers/DriverManager.java
@@ -0,0 +1,21 @@
+package managers;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverManager {
+
+  WebDriver driver;
+
+  public void initDriver(){
+    String browser = "";
+    switch (browser){
+      
+    }
+  }
+
+  public WebDriver getDriver() {
+    return driver;
+  }
+
+
+}
