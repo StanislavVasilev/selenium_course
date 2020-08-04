1645d9aeebb3351114b91171d53a5dd9d3fc8605
StanislavVasilev <svasilievalee@gmail.com>
2020-08-04 23:06:47 +0300

    "Исправлены пути к классу определения шагов, а так же в настройки"

--- src/main/java/task/managers/PropertyManager.java
+++ src/main/java/task/managers/PropertyManager.java
@@ -12,7 +12,7 @@ public class PropertyManager {
 
   public PropertyManager() {
     try {
-      properties.load(new FileInputStream(new File("src/main/resources" +
+      properties.load(new FileInputStream(new File("src/main/resources/" +
               System.getProperty("env", "application") + ".properties")));
     } catch (Exception e) {
       e.printStackTrace();
--- src/test/java/Cucumber/CucumberRunner.java
+++ src/test/java/Cucumber/CucumberRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources"}, glue = {"steps", "stepsDefinitions"}, plugin = {"pretty", "html:target/cucumber-html-report",
+@CucumberOptions(features = {"src/test/resources"}, glue = {"steps", "stepDefinitions"}, plugin = {"pretty", "html:target/cucumber-html-report",
         "json:target/cucumber-report.json"})
 
 public class CucumberRunner {
